21883f716aa7d75b8001b8cc407a260cf9ff4239
boblamceo <boblam.ceo@gmail.com>
2024-07-15 13:12:51 +0800

    create file

---
+++ music.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonApin, INPUT_PULLUP);
+  pinMode(buttonBpin, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
